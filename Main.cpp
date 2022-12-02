bcf44851a7a0d2daf1bfc3aaf0b5b1e28c5fb373
Abraham Mwaura <62555334+Abraham-Mwaura@users.noreply.github.com>
2022-12-02 22:18:20 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <iomanip>
+#include <cmath>
 float h=0.1, b=4.0,a=0, xK=0.0;
 
 int main()
@@ -8,8 +9,7 @@ int main()
     int n=((b-a)/h);
     float *Fk = new float(n);
     for (int i=1;i<=n;i++){
-        Fk[i]=(float)(16/(16+(float)(xK*xK)));
-        //cout<<Fk[i]<<" "<<xK<<endl;
+        Fk[i]=(float)(16/(16+(float)pow(xK,2)));
         xK=xK+h; 
     }
     float sum_oddFk=0;
@@ -17,13 +17,12 @@ int main()
     for (int j=2;j<n;j++){
     if(j%2!=0)
       {
-          //cout<<Fk[j]<<" "<<j;
           sum_oddFk = sum_oddFk+Fk[j];
       }
       else{
-         sum_evenFk = sum_evenFk+Fk[j];
+        sum_evenFk = sum_evenFk+Fk[j];
       }
     } 
-    float soltn=h/3*(Fk[0] +(4*sum_oddFk) +(2*sum_evenFk)+ Fk[n]);
-    cout<<soltn;
+    float soltn=h/3*(Fk[1] +(4*sum_oddFk) +(2*sum_evenFk)+ Fk[n]);
+    cout<<setprecision(5)<<soltn;
 }
